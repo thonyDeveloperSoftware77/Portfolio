667e0d5ad996c3efeef92fc6e627b10e776fbf1b
Anthony Cochea <thonydevelopersoftware@gmail.com>
2023-02-25 20:50:09 -0500

    Depurando codigo 1.3

--- components/SectionSkillsSrc/img/imgSkills.jsx
+++ components/SectionSkillsSrc/img/imgSkills.jsx
@@ -2,7 +2,7 @@ import reactImg from '../img/React.jpg';
 import animateCssImg from '../img/animateCss.jpg';
 import apexChartsImg from '../img/ApexChart.jpg';
 import axiosImg from '../img/Axios.jpg';
-import bootstrapImg from '../img/Bootstrap.jpg';
+import bootstrapImg from '../img/bootstrap.jpg';
 import forceGraphImg from '../img/forceGraph.jpg';
 import graphQlImg from '../img/graphQl.jpg';
 import leafletImg from '../img/leaftlet.jpg';
