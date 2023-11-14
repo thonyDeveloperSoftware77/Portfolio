ee619d9c9c2e0ba2613d58681cef513db6f93468
Anthony Cochea <thonydevelopersoftware@gmail.com>
2023-11-14 15:48:17 -0500

    Cambios

---
+++ components/SectionSkillsSrc/HardSkillsGraph.jsx
@@ -0,0 +1,147 @@
+import { useCallback, useEffect, useState, useRef } from "react";
+import ForceGraph3D from "react-force-graph-3d";
+import SpriteText from "three-spritetext";
+import * as d3 from "d3";
+
+
+const HardSkillsGraph = (props) => {
+  const fgRef = useRef(null);
+
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight
+  });
+
+  const data = {
+    nodes: [
+      { id: "Responsable" },
+      { id: "Trabajo en Equipo" },
+      { id: "Tolerante al Estrés" },
+      { id: "Sed de Aprender" },
+      { id: "Perfeccionista" },
+      { id: "Creativo" },
+
+      { id: 0, name: 'Lenguajes de Programación', type: "Category", label: "." },
+    ],
+    links: [
+      { source: "Responsable", target: 0, distance: 6 },
+      { source: "Trabajo en Equipo", target: 0, distance: 6 },
+      { source: "Tolerante al Estrés", target: 0, distance: 6 },
+      { source: "Sed de Aprender", target: 0, distance: 6 },
+      { source: "Perfeccionista", target: 0, distance: 6 },
+      { source: "Creativo", target: 0, distance: 6 },
+
+    ],
+  };
+  const [flag, setFlag] = useState(true);
+  const drawNormalNode = useCallback((node) => {
+    const sprite = new SpriteText(node.label ? node.label : node.id, 5);
+    sprite.color = "#ffffff";
+    sprite.backgroundColor = "transparent";
+    sprite.backgroundColor = "#90024c";
+    sprite.borderColor = "#65B5FF";
+    sprite.borderWidth = 0;
+    sprite.padding = [8, 4];
+    sprite.textHeight = 5;
+    sprite.borderWidth = 2;
+    sprite.borderRadius = 10;
+    sprite.fontWeight = 700;
+
+    return sprite;
+  }, []);
+
+  const drawCategoryNode = useCallback((node) => {
+    const sprite = new SpriteText(node.label ? node.label : node.id, 1);
+    sprite.color = "#ffffff";
+    sprite.backgroundColor = "#65B5FF";
+    sprite.padding = [5, 2];
+    sprite.textHeight = 1;
+    sprite.fontWeight = 100;
+    return sprite;
+  }, []);
+
+  const nodeThreeObject = useCallback(
+    (node) => {
+      if (node.type === "Category") {
+        return drawCategoryNode(node);
+      }
+      return drawNormalNode(node);
+    },
+    [drawCategoryNode, drawNormalNode]
+  );
+  useEffect(() => {
+    fgRef.current.d3Force("link").distance((link) => {
+      return link.distance ? link.distance * 20 : 20;
+    });
+
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight
+      });
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+
+
+  const handleClick = useCallback(
+    (node) => {
+      d3.selectAll("#node-info-container").remove();
+
+      const distance = 300;
+      const distRatio = 1 + distance / Math.hypot(node.x, node.y, node.z);
+      fgRef.current.cameraPosition(
+        { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }, // new position
+        node,
+        3000
+      );
+      setFlag(false);
+    },
+    [fgRef]
+  );
+
+  const handleBackgroundClick = useCallback(() => {
+    if (!flag) {
+      fgRef.current.cameraPosition({ x: -300, y: -300, z: -300 }, 0, 4000);
+      setFlag(true);
+      d3.selectAll("#node-info-container").remove();
+    } else {
+      return;
+    }
+  }, [fgRef]);
+
+  return (
+
+    <div style={{ marginTop: "-60px"}}>
+      <ForceGraph3D
+        width={windowSize.width <= 800 ? windowSize.width * 0.8 : windowSize.width * 0.25}
+        backgroundColor={"rgba(0, 0, 0, 0)"}
+        height={270}
+        ref={fgRef}
+        graphData={data}
+        nodeAutoColorBy="group"
+        nodeThreeObject={nodeThreeObject}
+        nodeVal={(node) => node.size}
+        linkColor={"color"}
+        linkOpacity={1}
+        linkWidth={"width"}
+        onNodeClick={handleClick}
+        onBackgroundClick={handleBackgroundClick}
+        onNodeDragEnd={(node) => {
+          node.fx = node.x;
+          node.fy = node.y;
+          node.fz = node.z;
+        }}
+
+      />
+    </div>
+
+  );
+}
+
+
+export default HardSkillsGraph;
--- pages/index.jsx
+++ pages/index.jsx
@@ -22,12 +22,24 @@ import { Html } from '@react-three/drei'
 import Loader from '../components/Loader';
 import { useMemo } from 'react'
 
+
+//Importar libreria de Apex chart con server side rending deshabilitado
+const ApexCharts = dynamic(() => import("react-apexcharts"), { ssr: false });
+const SoftSkillsGraph = dynamic(() => import('../components/SectionSkillsSrc/SoftSkillsGraph.jsx'), {
+  ssr: false,
+});
+
+const HardSkillsGraph = dynamic(() => import('../components/SectionSkillsSrc/HardSkillsGraph.jsx'), {
+  ssr: false,
+});
+
 /*Icons*/
 import { FaUniversity, FaLanguage } from 'react-icons/fa'
 import { MdWorkOutline } from 'react-icons/md'
 import { AiOutlineTrophy } from 'react-icons/ai'
 import { BsBookmarkStar } from 'react-icons/bs'
 import { FiUser } from 'react-icons/fi'
+import { BiExit } from 'react-icons/bi'
 
 const Textures = dynamic(() => import('../components/Textures'))
 
@@ -56,7 +68,10 @@ export default function Home() {
     2: false,
     3: false,
     4: false,
+    5: false,
   })
+
+  const [salir, setSalir] = useState(false)
 
   function handleClickOpciones(newOption) {
 
@@ -64,24 +79,30 @@ export default function Home() {
       setIsLoading(!isLoading);
       setTimeout(() => {
         setopciones(newOption);
-      }, 2000);
+      }, 2500);
     }
   }
   const handleClickOpcionesCv = (index) => {
-    if (index === 6) {
-      setOpcionesCv({
-        0: false,
-        1: false,
-        2: false,
-        3: false,
-        4: false,
-      });
-    } else {
-      setOpcionesCv(prevState => ({
-        ...prevState,
-        [index]: !prevState[index]
-      }));
+    if (index == 6) {
+      setSalir(true)
+      setTimeout(() => {
+        setSalir(false)
+        setOpcionesCv(prevState => ({
+          ...prevState,
+          [0]: false,
+          [1]: false,
+          [2]: false,
+          [3]: false,
+          [4]: false,
+          [5]: false,
+        }));
+      }, 600);
+
     }
+    setOpcionesCv(prevState => ({
+      ...prevState,
+      [index]: !prevState[index]
+    }));
   }
 
   function hadleClickOpcionesAnimacion() {
@@ -103,6 +124,39 @@ export default function Home() {
   }, [opciones])
 
 
+
+
+  const optionsNivelRiesgo = {
+    chart: {
+      height: 350,
+      type: "radialBar",
+    },
+    plotOptions: {
+      radialBar: {
+        dataLabels: {
+          name: {
+            fontSize: "22px",
+          },
+          value: {
+            color: "#fff", // color del texto del valor
+          },
+        },
+      },
+    },
+    fill: {
+      colors: [
+        function ({ value }) {
+          if (value < 100) {
+            return "#02abe4"; // verde
+          }
+        },
+      ],
+    },
+    labels: ["Ingles"],
+  };
+
+  const seriesLanguage = [70];
+
   return (
     <>
 
@@ -117,21 +171,18 @@ export default function Home() {
 
         </nav>
 
-
-
         <div style={{ position: "fixed", width: "100%" }} >
           {opciones === 0 ? (
             <>
+
               <div style={{ width: "100%", height: "100vh" }}>
+                {/*
                 <Canvas onClick={(e) => hadleClickOpcionesAnimacion(e)}>
                   <Suspense fallback={<Loading />}>
                     <Objeto3D animation={opcionAnimacion} />
-
                     <hemisphereLight intensity={0.3} color={"#0000FF"} />
-
                     <Textures />
                     <Environment resolution={512}>
-                      {/* Ceiling */}
                       <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, -9]} scale={[10, 1, 1]} />
                       <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, -6]} scale={[10, 1, 1]} />
                       <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, -3]} scale={[10, 1, 1]} />
@@ -139,15 +190,14 @@ export default function Home() {
                       <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 3]} scale={[10, 1, 1]} />
                       <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 6]} scale={[10, 1, 1]} />
                       <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 9]} scale={[10, 1, 1]} />
-                      {/* Sides */}
                       <Lightformer intensity={2} rotation-y={Math.PI / 2} position={[-50, 2, 0]} scale={[100, 2, 1]} />
                       <Lightformer intensity={2} rotation-y={-Math.PI / 2} position={[50, 2, 0]} scale={[100, 2, 1]} />
-                      {/* Key */}
                       <Lightformer form="ring" color="blue" intensity={15} scale={2} position={[10, 5, 10]} onUpdate={(self) => self.lookAt(0, 0, 0)} />
                     </Environment>
                     <OrbitControls />
                   </Suspense>
                 </Canvas>
+                */}
               </div>
 
               <div className='NameContainer'>
@@ -210,113 +260,238 @@ export default function Home() {
 
 
               </div>
-              {opcionesCv[0] ? (<div className='CardContainer'>
-                <button onClick={() => handleClickOpcionesCv(6)} >Cerrar</button>
-                <center>
-                  <h2>PROFILE</h2>
-                </center>
-              </div>) : (<></>)}
-              {opcionesCv[1] ? (<div className='CardContainer'>
-                <button onClick={() => handleClickOpcionesCv(6)} >Cerrar</button>
+              {opcionesCv[0] ? (
+                <div className={`CardContainer ${salir ? 'Inactive' : ''}`}>
+                  <button className='buttonExit' onClick={() => handleClickOpcionesCv(6)} >
+                    <BiExit size={25} />
+                  </button>
+                  <center>
+                    <h2>PROFILE</h2>
+                    <br />
+                    <p>Como apasionado del mundo tecnológico, he adquirido una amplia gama de 
+                      habilidades que abarcan desde el desarrollo de aplicaciones web y móviles 
+                      multiplataforma, hasta el diseño de interfaces de usuario
+                       y la edición. 
+                       <br />
+                       <br />
+                       Mi experiencia también se extiende al manejo de arquitecturas para
+                       desarrollar software de calidad, así como a la gestión de proyectos, 
+                       bases de datos  y hacking ético. He trabajado en varios proyectos, 
+                       manteniéndome siempre al día con las tecnologías más actuales. 
+                       <br />
+                       <br />
+                       Mi objetivo es utilizar estas habilidades para crear soluciones innovadoras 
+                       y efectivas en el campo de la tecnología.</p>
+                    <p></p>
+                  </center>
+                </div>
+              ) : (<></>)}
+              {opcionesCv[1] ? (<div className={`CardContainer ${salir ? 'Inactive' : ''}`}>
+                <button className='buttonExit' onClick={() => handleClickOpcionesCv(6)} ><BiExit size={25} /></button>
                 <center>
                   <h2>EDUCATION</h2>
+                  <p>Formación</p>
+                  <p>Ingeniería de Software - UDLA</p>
+                  <img src="/img/udla.png" alt="Vercel Logo" width={94} height={49} />
+
+                  <br />
+                  <p>Certificados</p>
+                  <div className='flexContainer'>
+                    <div className='flexRow'>
+                      <div className='txtLeft'>
+                        The Complete Web Development
+                        <br />
+                        Dr. Angela Yu-London Brewery
+                      </div>
+                      <div className='txtRigth'>
+                        2023
+                      </div>
+                    </div>
+
+                    <div className='flexRow'>
+                      <div className='txtLeft'>
+                        CCNA(CiscoCertifiedNetworkAssociate)
+                        <br />
+                        Cisco Networking Academy
+                      </div>
+                      <div className='txtRigth'>
+                        2023
+                      </div>
+                    </div>
+                    <div className='flexRow'>
+                      <div className='txtLeft'>
+                        Design Thinking for innovation
+                        <br />
+                        University of Virginia
+                      </div>
+                      <div className='txtRigth'>
+                        2021
+                      </div>
+                    </div>
+                    <div className='flexRow'>
+                      <div className='txtLeft'>
+                        Linux Unhatched
+                        <br />
+                        NDG Linux Unhatched
+                      </div>
+                      <div className='txtRigth'>
+                        2022
+                      </div>
+                    </div>
+
+                    <div className='flexRow'>
+                      <div className='txtLeft'>
+                        Experiencia de usuario UX/UI + Figma
+                        <br />
+                        Udemy Business
+                      </div>
+                      <div className='txtRigth'>
+                        2023
+                      </div>
+                    </div>
+
+                    <div className='flexRow'>
+                      <div className='txtLeft'>
+                        Next.js
+                        <br />
+                        Udemy Business
+                      </div>
+                      <div className='txtRigth'>
+                        2023
+                      </div>
+                    </div>
+                    <div className='flexRow'>
+                      <div className='txtLeft'>
+                        GIT+GitHub: Sistema de control
+                        de versiones
+                        <br />
+                        Udemy Business
+                      </div>
+                      <div className='txtRigth'>
+                        2023
+                      </div>
+                    </div>
+                  </div>
                 </center>
               </div>) : (<></>)}
 
-              {opcionesCv[2] ? (<div className='CardContainer'>
-                <button onClick={() => handleClickOpcionesCv(6)} >Cerrar</button>
+              {opcionesCv[2] ? (<div className={`CardContainer ${salir ? 'Inactive' : ''}`}>
+                <button className='buttonExit' onClick={() => handleClickOpcionesCv(6)} ><BiExit size={25} /></button>
                 <center>
                   <h2>SKILLS</h2>
+                  <p>Soft Skills</p>
+                  <SoftSkillsGraph />
+                  <p>Hard Skills</p>
+                  <HardSkillsGraph />
                 </center>
               </div>) : (<></>)}
 
-              {opcionesCv[3] ? (<div className='CardContainer'>
-                <button onClick={() => handleClickOpcionesCv(6)} >Cerrar</button>
+              {opcionesCv[3] ? (<div className={`CardContainer ${salir ? 'Inactive' : ''}`}>
+                <button className='buttonExit' onClick={() => handleClickOpcionesCv(6)} ><BiExit size={25} /></button>
                 <center>
                   <h2>ACHIEVEMENTS</h2>
+                  <h3>Logros Académicos</h3>
+                  <p>Certificado de Reconocimiento de UDLA por autoaprendizaje de cursos de software en Udemy Business</p>
+                  <p>Diploma de distinción: 1er Escolta del Pabellón Nacional</p>
+                  <h3>Logros Personales</h3>
+                  <p>Partícipe del título de campeones en dos ocasiones consecutivas con los equipos de fútbol Racing y Sao Paulo en Santa Elena.</p>
+                  <p>Demostrar mis habilidades musicales tocando la guitarra eléctrica en varios eventos escolares, lo cual ha sido una experiencia enriquecedora y gratificante</p>
+                  <p>Ganador de concursos de Dibujo</p>
                 </center>
               </div>) : (<></>)}
 
-              {opcionesCv[4] ? (<div className='CardContainer'>
-                <button onClick={() => handleClickOpcionesCv(6)} >Cerrar</button>
+              {opcionesCv[4] ? (<div className={`CardContainer ${salir ? 'Inactive' : ''}`}>
+                <button className='buttonExit' onClick={() => handleClickOpcionesCv(6)} ><BiExit size={25} /></button>
                 <center>
                   <h2>WORK EXPERIENCE</h2>
+                  <br />
+                  <p>
+                    Sistema de Auditorías AuditSafe Consulting
+                  </p>
                 </center>
               </div>) : (<></>)}
 
-              {opcionesCv[5] ? (<div className='CardContainer'>
-                <button onClick={() => handleClickOpcionesCv(6)} >Cerrar</button>
+              {opcionesCv[5] ? (<div className={`CardContainer ${salir ? 'Inactive' : ''}`}>
+                <button className='buttonExit' onClick={() => handleClickOpcionesCv(6)} ><BiExit size={25} /></button>
                 <center>
                   <h2>LANGUAGES</h2>
+                  <div>
+                    <ApexCharts
+                      options={optionsNivelRiesgo}
+                      series={seriesLanguage}
+                      type="radialBar"
+                      height={350}
+                    />
+                  </div>
                 </center>
               </div>) : (<></>)}
 
 
 
 
-                <div>
-                  <div className='titleSection'>
-                    THONY DEVELOPER
-                  </div>
-                  <div className='subtitleSection'>
-                    Desarrollador de sitios y aplicaciones web, ingeniero de software y apasionado por la tecnología.
-                  </div>
+              <div>
+                <div className='titleSection'>
+                  THONY DEVELOPER
                 </div>
-              </>
-              ) : opciones === 1 ? (
-                <div>
-                  <ThreeScene2 componente={SectionProjects} />
+                <div className='subtitleSection'>
+                  Desarrollador de sitios y aplicaciones web, ingeniero de software y apasionado por la tecnología.
                 </div>
-              ) : opciones === 2 ? (
-                <div>
-                  <ThreeScene2 componente={SectionSkills} />
-                </div>
-              ) : opciones === 3 ? (
-                <div>
-                  <ThreeScene2 componente={SectionAboutMe} />
-                </div>
-              ) : opciones === 5 ? (
-                <div>
-                  <ThreeScene2 componente={SectionContact} />
-                </div>
-              ) : (
-                <p>No option selected</p>
-              )}
-              { }
+              </div>
+            </>
+          ) : opciones === 1 ? (
+            <div>
+              <ThreeScene2 componente={SectionProjects} />
             </div>
-
-
-          <div className={` ${opciones !== 0 ? 'boxOptionesUp' : 'boxOptions'}`}>
-            <a className={opciones === 0 ? 'selectedOption' : ''} onClick={() => { handleClickOpciones(0) }}>Inicio</a>
-            <a className={opciones === 1 ? 'selectedOption' : ''} onClick={() => { handleClickOpciones(1) }}>Proyectos</a>
-            <a className={opciones === 2 ? 'selectedOption' : ''} onClick={() => { handleClickOpciones(2) }}>Skills</a>
-            <a className={opciones === 3 ? 'selectedOption' : ''} onClick={() => { handleClickOpciones(3) }}>About Me</a>
-            <a className={opciones === 5 ? 'selectedOption' : ''} onClick={() => { handleClickOpciones(5) }}>Contacto</a>
-
-          </div>
-
+          ) : opciones === 2 ? (
+            <div>
+              <ThreeScene2 componente={SectionSkills} />
+            </div>
+          ) : opciones === 3 ? (
+            <div>
+              <ThreeScene2 componente={SectionAboutMe} />
+            </div>
+          ) : opciones === 5 ? (
+            <div>
+              <ThreeScene2 componente={SectionContact} />
+            </div>
+          ) : (
+            <p>No option selected</p>
+          )}
+          { }
         </div>
 
 
+        <div className={` ${opciones !== 0 ? 'boxOptionesUp' : 'boxOptions'}`}>
+          <a className={opciones === 0 ? 'selectedOption' : ''} onClick={() => { handleClickOpciones(0) }}>Inicio</a>
+          <a className={opciones === 1 ? 'selectedOption' : ''} onClick={() => { handleClickOpciones(1) }}>Proyectos</a>
+          <a className={opciones === 2 ? 'selectedOption' : ''} onClick={() => { handleClickOpciones(2) }}>Skills</a>
+          <a className={opciones === 3 ? 'selectedOption' : ''} onClick={() => { handleClickOpciones(3) }}>About Me</a>
+          <a className={opciones === 5 ? 'selectedOption' : ''} onClick={() => { handleClickOpciones(5) }}>Contacto</a>
 
-        <main className={styles.main}>
+        </div>
+
+      </div >
 
 
 
-        </main>
+      <main className={styles.main}>
 
-        <footer className={styles.footer}>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by{' '}
-            <span className={styles.logo}>
-              <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-            </span>
-          </a>
-        </footer>
-      </>
-      )
+
+
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
+    </>
+  )
 }
